337331640476ecb3378b69de45e9f090c96056a5
makotovnjp <makotovnjp@gmail.com>
2025-08-12 22:45:04 +0900

    update

--- mujoco_playground/_src/locomotion/hunter/default_config.py
+++ mujoco_playground/_src/locomotion/hunter/default_config.py
@@ -3,6 +3,11 @@ from ml_collections import config_dict
 def default_config():
     config = config_dict.ConfigDict()
     config.sim_dt = 0.002
+    config.ctrl_dt = 0.02
     config.impl = 'mjx'
+    config.episode_length = 1000
+    config.action_repeat = 1
+    config.action_scale = 0.5
+    config.obs_noise = 0.0
     # Add more default config values as needed for Hunter
     return config
