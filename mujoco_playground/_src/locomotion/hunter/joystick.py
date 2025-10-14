34ca1fc4059ca3c769f221252a8d2dfcbb9694e5
Trung Pham <brain10master@gmail.com>
2025-10-14 17:16:26 +0900

    Change reward for fine-tuning

--- mujoco_playground/_src/locomotion/hunter/joystick.py
+++ mujoco_playground/_src/locomotion/hunter/joystick.py
@@ -81,12 +81,12 @@ def default_config() -> config_dict.ConfigDict:
       reward_config=config_dict.create(
           scales=config_dict.create(
               # Rewards.
-              # feet_phase=5.0,
+              feet_phase=5.0,
               tracking_lin_vel=3.5,
               tracking_ang_vel=0.75,
               # feet_air_time=2.0,
 
-              feet_phase=3.0,
+            #   feet_phase=3.0,
               # tracking_lin_vel=0.0,
               # tracking_ang_vel=0.0,
               feet_air_time=2.0,
@@ -107,14 +107,17 @@ def default_config() -> config_dict.ConfigDict:
               termination=-1.0,
               foot_slip=-0.1,
               action_rate=-0.01,  # previous: -0.5
-              feet_distance=-0.3,
+            #   action_rate=-0.1,
+              feet_distance=-0.0,
           ),
           tracking_sigma=0.5,
       ),
       command_config=config_dict.create(
           lin_vel_x=[-1.0, 1.0],
-          lin_vel_y=[-1.0, 1.0],
-          ang_vel_yaw=[-1.0, 1.0],
+        #   lin_vel_y=[-1.0, 1.0],
+        #   ang_vel_yaw=[-1.0, 1.0],
+          lin_vel_y=[-1.2, 1.2],
+          ang_vel_yaw=[-2*np.pi, 2*np.pi],
       ),
       push_config=config_dict.create(
           enable=True,
@@ -123,11 +126,12 @@ def default_config() -> config_dict.ConfigDict:
       ),
       # gait_frequency=[0.25, 2.0],
       # gait_frequency=[0.0, 0.5],
-      gait_frequency=[0.5, 4.0],
-      # gaits=["walk"],
-      gaits=["walk", "stand"],
+      gait_frequency=[1.5, 1.5],
+      gaits=["walk"],
+    #   gaits=["walk", "stand"],
       # gaits=["walk","stand","run"],
-      foot_height=[0.08, 0.4],
+    #   foot_height=[0.08, 0.4],
+      foot_height=[0.1, 0.1],
       impl="jax",
       nconmax=8 * 1024,
       njmax=10 + 8 * 4,
@@ -545,9 +549,9 @@ class Joystick(hunter_base.HunterEnv):
     state = jp.hstack(
       [
         state,
-        info["gait"],  # 1
-        info["gait_freq"],  # 1
-        info["foot_height"],  # 1
+        # info["gait"],  # 1
+        # info["gait_freq"],  # 1
+        # info["foot_height"],  # 1
       ]
     )  # total: 46
 
