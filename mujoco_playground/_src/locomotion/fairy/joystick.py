d21fbef354f2fb9199cce464a8f3bea1093ad416
chuoru <tranvietthanh0101@gmail.com>
2025-11-04 01:29:05 +0900

    feat: update termination condition

--- mujoco_playground/_src/locomotion/fairy/joystick.py
+++ mujoco_playground/_src/locomotion/fairy/joystick.py
@@ -383,7 +383,7 @@ class Joystick(fairy_base.FairyEnv):
     return state
 
   def _get_termination(self, data: mjx.Data) -> jax.Array:
-    fall_termination = self.get_gravity(data)[-1] < 0.0
+    fall_termination = self.get_gravity(data)[-1] < 0.85
     return (
         fall_termination | jp.isnan(data.qpos).any() | jp.isnan(data.qvel).any()
     )
